9d5787effa6125b4f21bbabbc8bb620dcdee4be5
SonoLeon <leonpastorellic@gmail.com>
2025-02-17 02:57:24 +0100

    fix

--- src/main/java/it/univaq/f4i/iw/ex/webmarket/data/dao/impl/RichiestaDAO_MySQL.java
+++ src/main/java/it/univaq/f4i/iw/ex/webmarket/data/dao/impl/RichiestaDAO_MySQL.java
@@ -68,21 +68,25 @@ public class RichiestaDAO_MySQL extends DAO implements RichiestaDAO {
                 "WHERE id=? AND version=?"
             );
             sRichiesteSenzaProposte = connection.prepareStatement(
-            "SELECT r.id, r.note, r.stato, r.data, r.codice_richiesta, r.ordinante, r.tecnico, r.categoria " +
-                "FROM richiesta r " +
-                "WHERE r.stato = ? AND r.tecnico = ? " +
-                "AND EXISTS (SELECT 1 FROM proposta p WHERE p.richiesta_id = r.id AND (p.stato = 'ACCETTATO' or p.stato = 'IN_ATTESA')) ORDER BY data ASC"
-            ); 
+    "SELECT r.id, r.note, r.stato, r.data, r.codice_richiesta, r.ordinante, r.tecnico, r.categoria " +
+    "FROM richiesta r " +
+    "WHERE r.stato = ? AND r.tecnico = ? " +
+    "  AND NOT EXISTS (SELECT 1 FROM proposta p " +
+    "                  WHERE p.richiesta_id = r.id AND p.stato <> 'RIFIUTATO') " +
+    "ORDER BY r.data ASC"
+);
+
             
             // PreparedStatement per recuperare le richieste prese in carico da un tecnico
             sRichiestePreseInCaricoConProposteByTecnico = connection.prepareStatement(
-                "SELECT r.id, r.note, r.stato, r.data, r.codice_richiesta, r.ordinante, r.tecnico, r.categoria " +
-                "FROM richiesta r " +
-                "WHERE r.stato = ? AND r.tecnico = ? " +  
-                "AND EXISTS (SELECT 1 FROM proposta p WHERE p.richiesta_id = r.id) " + //Richieste con ALMENO una proposta
-                "ORDER BY r.data ASC"
-            
-            );          
+    "SELECT r.id, r.note, r.stato, r.data, r.codice_richiesta, r.ordinante, r.tecnico, r.categoria " +
+    "FROM richiesta r " +
+    "WHERE r.stato = ? AND r.tecnico = ? " +
+    "  AND EXISTS (SELECT 1 FROM proposta p " +
+    "              WHERE p.richiesta_id = r.id AND p.stato <> 'RIFIUTATO') " +
+    "ORDER BY r.data ASC"
+);
+
             // PreparedStatement per recuperare le richieste in attesa 
             sRichiesteInAttesa = connection.prepareStatement("SELECT * FROM richiesta WHERE stato = ?");
             // Prepariamo la query che conta quante proposte non rifiutate esistono per una certa richiesta
